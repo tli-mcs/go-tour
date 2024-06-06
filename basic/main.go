5dec955755744cebf4d869c17398177fee0aa691
Ting Li <tli@macquariecloudservices.com>
2024-06-06 22:10:36 +1000

    done

--- basic/main.go
+++ basic/main.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello from Go! ")
+	fmt.Println(Ex001(2000, 3200))
+}
+
+func Ex001(low, high int) string {
+	var numbers []string
+	for i := low; i <= high; i++ {
+		if i%7 == 0 && i%5 != 0 {
+			numbers = append(numbers, strconv.Itoa(i))
+		}
+	}
+	return strings.Join(numbers, ",")
 }
