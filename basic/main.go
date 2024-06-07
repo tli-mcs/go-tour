7db468058fa6a6d24ea846a5a6ad1f6583aef7a1
Ting Li <tli@macquariecloudservices.com>
2024-06-07 11:58:31 +1000

    done

--- basic/main.go
+++ basic/main.go
@@ -47,3 +47,17 @@ func Ex003(input int) map[int]int {
 	}
 	return result
 }
+
+func Ex004(input string) []int {
+	// create a map with the size of n
+	numbers := strings.Split(input, ",")
+
+	length := len(numbers)
+	var num = make([]int, length)
+
+	for index, v := range numbers {
+		s := strings.Trim(v, " ")
+		num[index], _ = strconv.Atoi(s)
+	}
+	return num
+}
