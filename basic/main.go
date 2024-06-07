1739621cd7795ffefcaf8c36a2dc19aac8c092f0
Ting Li <tli@macquariecloudservices.com>
2024-06-07 11:45:06 +1000

    done

--- basic/main.go
+++ basic/main.go
@@ -8,25 +8,15 @@ import (
 )
 
 func main() {
-	fmt.Println("Exercise 002")
-
-	// reading input from console
-	var input int
-	fmt.Print("Please enter a number : ")
-	_, err := fmt.Scanln(&input)
-
-	// checking for error and low 0
+	fmt.Println("Exercise 003")
+	var n int
+	fmt.Print("Enter a number: ")
+	_, err := fmt.Scanln(&n)
 	if err != nil {
-		log.Fatal("Please enter a number")
+		log.Fatal("Error occured: ", err)
 	}
 
-	result, err := Ex002(input)
-
-	if err != nil {
-		log.Fatalf("Error for input %v: %v", input, err)
-	}
-
-	fmt.Printf("Factorial of %v = %v", input, result)
+	fmt.Printf("%v", Ex003(n))
 }
 
 func Ex001(low, high int) string {
@@ -48,4 +38,12 @@ func Ex002(input int) (uint64, error) {
 		result *= uint64(i)
 	}
 	return result, nil
+}
+
+func Ex003(input int) map[int]int {
+	result := make(map[int]int)
+	for i := 1; i <= input; i++ {
+		result[i] = i * i
+	}
+	return result
 }
