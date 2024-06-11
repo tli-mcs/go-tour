e0c109decd14c963bf4e63292d82bf68da43e521
Ting Li <tli@macquariecloudservices.com>
2024-06-11 14:29:58 +1000

    done

--- methodspoc/main.go
+++ methodspoc/main.go
@@ -1,33 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 type Vertex struct {
 	X, Y float64
 }
 
-func (v *Vertex) Scale(f float64) {
-	v.X = v.X * f
-	v.Y = v.Y * f
+func (v Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
-func ScaleFunc(v *Vertex, f float64) {
-	v.X = v.X * f
-	v.Y = v.Y * f
+func AbsFunc(v Vertex) float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
 func main() {
 	v := Vertex{3, 4}
-	fmt.Println(v)
-	v.Scale(2)
-	fmt.Println(v)
-	ScaleFunc(&v, 10)
-	fmt.Println(v)
+	fmt.Println(v.Abs())
+	fmt.Println(AbsFunc(v))
 
-	// p := &Vertex{4, 3}
-	p := &v
-	p.Scale(3)
-	ScaleFunc(p, 8)
-
-	fmt.Println(v, p)
+	p := &Vertex{4, 3}
+	fmt.Println(p.Abs())
+	fmt.Println(AbsFunc(*p))
 }
