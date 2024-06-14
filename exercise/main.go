f7caf03ae19c5c191d7610e221da72257de468c0
Ting Li <tli@macquariecloudservices.com>
2024-06-14 19:56:48 +1000

    done

--- exercise/main.go
+++ exercise/main.go
@@ -1,13 +1,21 @@
-// Exercise: Pointers
+// Exercise: Functions
 
-// Print the address of a variable
+// Create a variadic function that sums many numbers passed as arguments
+// Variadic functions can be called with any number of arguments.
+
 package main
 
 import "fmt"
 
+func calc(numbers ...int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
+
 func main() {
-	var x int = 5
 	// Your code goes here
-	var p = &x
-	fmt.Println(p)
+	fmt.Println(calc(1, 2, 3, 4, 5))
 }
