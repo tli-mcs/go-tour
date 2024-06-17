737c02b9a2ce8f0f5aa58b8bcb499049dd43f4a2
Ting Li <tli@macquariecloudservices.com>
2024-06-17 13:36:32 +1000

    done

--- ginpoc/main.go
+++ ginpoc/main.go
@@ -2,12 +2,20 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+type Person struct {
+	ID   string `uri:"id" binding:"required,uuid"`
+	Name string `uri:"name" binding:"required"`
+}
+
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+	route := gin.Default()
+	route.GET("/:name/:id", func(c *gin.Context) {
+		var person Person
+		if err := c.ShouldBindUri(&person); err != nil {
+			c.JSON(400, gin.H{"msg": err.Error()})
+			return
+		}
+		c.JSON(200, gin.H{"name": person.Name, "uuid": person.ID})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	route.Run(":8088")
 }
