63ca4c30a6f09b1f716c24a20be82a30236c1fd5
Ting Li <tli@macquariecloudservices.com>
2024-06-17 16:00:43 +1000

    done

--- exercise/main.go
+++ exercise/main.go
@@ -1,21 +1,31 @@
-// Exercise: Functions
+// Exercise: Routines
 
-// Create a variadic function that sums many numbers passed as arguments
-// Variadic functions can be called with any number of arguments.
+// Document yourself and ask what a goroutine is.
+// Make a go routine of the counter() function
+// right after calling the go routine, in the next line. call the same routine with another different number
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"time"
+)
 
-func calc(numbers ...int) int {
-	sum := 0
-	for _, number := range numbers {
-		sum += number
+func counter(x int, y string) {
+	for i := 0; i < x; i++ {
+		fmt.Println(y + " print: " + strconv.Itoa(i))
+		time.Sleep(1 * time.Second)
 	}
-	return sum
 }
 
 func main() {
 	// Your code goes here
-	fmt.Println(calc(1, 2, 3, 4, 5))
+	go counter(10, "goroutine 1")
+	counter(10, "main routine")
+	go counter(10, "goroutine 2")
+
+	// this sleep is in order to not exit the program sooner than the routine lifetime :)
+	time.Sleep(20 * time.Second)
+	fmt.Println("All finished")
 }
