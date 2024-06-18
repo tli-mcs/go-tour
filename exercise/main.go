8c8096357d4346d47406aa2b55a8259e2c456942
Ting Li <tli@macquariecloudservices.com>
2024-06-18 17:02:33 +1000

    done

--- exercise/main.go
+++ exercise/main.go
@@ -1,34 +1,20 @@
-// Exercise: Channels (buffered)
+// Exercise: Channels directions (only read/rx)
 
-// Channels that accept a single message (<-) are synchronous. But go also can use async channels, or buffered channels
-// Create a buffered string channel with a capacity of 4
-// Send directly 4 different strings to that channel.
-// Use the pop_message function 4 times to unbuffer the channel and see how it works
+// Make a goroutine with a channel for only receive data.
+// The function should be called "receive" and the receive-only channel should be it's 1st and only argument
+// Sending data from that channel is prohibited / will cause compiler errors
+// Feed some string into that channel.
 
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
-func pop_message(c chan string) {
-	msg := <-c
-	fmt.Println(msg)
+func receive(c <-chan string) {
+	fmt.Println(<-c)
 }
+
 func main() {
-	// Your code goes here
-	var c chan string = make(chan string, 4)
-	c <- "My"
-	c <- "Name"
-	c <- "is"
-	c <- "Enin"
-
-	pop_message(c)
-	pop_message(c)
-	pop_message(c)
-	pop_message(c)
-
-	// this sleep is in order to not exit the program sooner than the routine lifetime :)
-	time.Sleep(1 * time.Second)
+	var c chan string = make(chan string, 1)
+	c <- "HelloWorld!"
+	receive(c)
 }
