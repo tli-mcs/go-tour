0afed4a2cdf41a4c9aa20df4fd0ec9ff0fac91b1
Ting Li <tli@macquariecloudservices.com>
2024-06-19 19:39:58 +1000

    done

--- exercise/main.go
+++ exercise/main.go
@@ -1,21 +1,19 @@
-// Exercise: Channels directions (only send/tx)
+// Exercise: Channels - Closing
 
-// Make a goroutine with a channel for only send data.
-// The function should be called "send" and the send-only channel should be it's 1st and only argument
-// Receive data from that channel is prohibited / will cause compiler errors
+// Create a string channel "c" (make it a buffered channel)
+// Add 2 different strings directly into that channel.
+// Close the channel with the close() statement and read a quote from the channel, Can you read it?
 
 package main
 
 import "fmt"
 
-func send(c chan<- string) {
-	c <- "Only send"
-}
-
 func main() {
-	var c chan string = make(chan string, 2)
-	c <- "hahah"
-	send(c)
+	c := make(chan string, 2)
+	c <- "Hello"
+	c <- "World"
 	fmt.Println(<-c)
-	fmt.Println(<-c)
+	close(c)
+	c <- "abc"
+	fmt.Println(<-c) // panic: send on closed channel
 }
