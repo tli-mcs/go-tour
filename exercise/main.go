7699df7bd52724b6a10c23b7770dd476e381fe5c
Ting Li <tli@macquariecloudservices.com>
2024-06-20 15:44:35 +1000

    done

--- exercise/main.go
+++ exercise/main.go
@@ -1,39 +1,28 @@
-// Exercise: JSON!! - Unknown beforehand data
-
-// Now the JSON will be unstructured (we won't know the values beforehand)
-// When data is unstructured, we will create a map of strings to empty interfaces.
-// Create a map of string to an empty interface called "result"
-// Unmarshall (decode) the json into the result
-// Get the "birds" object into a variable called "birds" and print it's values
-
+// Contexts - Chaining contexts, mutable or immutable? It's Immutable!
+// contexts are wrapped within contexts and immutable
 package main
 
 import (
-	"encoding/json"
+	"context"
 	"fmt"
 )
 
+func doAnother(ctx context.Context) {
+	fmt.Println("doSomething: Name's value is", ctx.Value("Name"))
+}
 
+// Modify the doSomething function
+func doSomething(ctx context.Context) {
+	fmt.Println("doSomething: Name's value is", ctx.Value("Name"))
+	anotherCtx := context.WithValue(ctx, "Name", "Mary")
+	doAnother(anotherCtx)
+	// Add another print statement to show the ctx.Value for your key again, does it change?
+	fmt.Println("doSomething: Name's value is", ctx.Value("Name"))
+
+}
 
 func main() {
-  birdJson := `{
-                "birds": {
-                  "pigeon":"likes to perch on rocks",
-                  "eagle":"bird of prey"
-                },
-                "animals": "none"
-              }`
-
-  var result map[string]interface{}
-  json.Unmarshal([]byte(birdJson), &result)
-  
-  // The object stored in the "birds" key is also stored as 
-  // a map[string]interface{} type, and its type is asserted from
-  // the interface{} type
-  birds := result["birds"].(map[string]interface{})
-  
-  for key, value := range birds {
-    // Each value is an interface{} type, that is type asserted as a string
-    fmt.Println(key, value.(string))
-  }
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, "Name", "John")
+	doSomething(ctx)
 }
