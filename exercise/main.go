e3d9d14f0358ce2f2629bd0b1edb17553f4c24cb
Ting Li <tli@macquariecloudservices.com>
2024-06-20 16:45:44 +1000

    done

--- exercise/main.go
+++ exercise/main.go
@@ -1,65 +1,38 @@
-// Contexts - Canceling a context (withDeadline)
+// Interfaces - Intro - Implicit implementation
+
+// In golang, we don't use the "implements" keyword when using an interface.
+// We don't have to say that explicitly, instead we need some type to satisfy the area() method.
+// We say something implements an interface if it has a method with the exact signature.
+// Inside our geometry interface, we have a signature called "area()"
+// In this exercise, we are going to create a function with that signature!
+
 package main
 
 import (
-	"context"
 	"fmt"
-	"time"
 )
 
-// Modify the doSomething function
-func doSomething(ctx context.Context) {
-	// Deadline of 1,5 seconds
-	deadline := time.Now().Add(1500 * time.Millisecond)
-	// Repeat the same operation for cancelling the context, but this time instead of WithCancel() we will use WithDeadline(a,b) the a argument will be the parent context ctx
-	// and the b argument will be the deadline above (1,5 seconds in this case)
-	ctx, cancelCtx := context.WithDeadline(ctx, deadline)
-	// defer cancelCtx when time has passed
-	defer cancelCtx()
-	// Make a new unbuffered channel of integers and assign it to printCh
-	printCh := make(chan int)
-	// call the doAnother function as goroutine
-	go doAnother(ctx, printCh)
-	// Modify the loop, for each number, wait for one second.
-	// And whenever it receives a ctx.Done(), just break the selection.
-outerLoop:
-	for num := 1; num <= 5; num++ {
-		select {
-		// case 1 (receive a number to printCh channel)
-		// then sleep for a second
-		case printCh <- num:
-			time.Sleep(500 * time.Millisecond)
-		// case 2 (received ctx.done())
-		case <-ctx.Done():
-			fmt.Printf("doSomething: %s\n", ctx.Err())
-			break outerLoop
-		}
-	}
-
-	// sleep for 100 ms
-	time.Sleep(1000 * time.Millisecond)
-	// print that doSomething has finished
-	fmt.Printf("doSomething: finished\n")
+type geometry interface {
+	area() float64
 }
 
-func doAnother(ctx context.Context, printCh <-chan int) {
-	for {
-		select {
-		// first case will be reciving a ctx.Done() call, if this happens we will handle errors and abort the doAnother function.
-		case <-ctx.Done():
-			if err := ctx.Err(); err != nil {
-				fmt.Printf("doAnother err: %s\n", err)
-			}
-			fmt.Printf("doAnother: finished\n")
-			return
-		// Second case will receive a value from printCh and assign that to a variable called num, after that print the num variable
-		case num := <-printCh:
-			fmt.Printf("doAnother: %d\n", num)
-		}
-	}
+// A rectangle struct
+type rect struct {
+	width, height float64
+}
+
+// We are going to create a function with the area() signature.
+// The function will be a receiver function (the receiver will be a rectangle "r")
+// The function will be named area, without any arguments and will return a float64 value
+
+// The return value will be the area of the rectangle
+func (r rect) area() float64 {
+	return r.width * r.height
 }
 
 func main() {
-	ctx := context.Background()
-	doSomething(ctx)
+	// Create a new rectangle and store it in a variable
+	r := rect{width: 3, height: 4}
+	// Print the variable
+	fmt.Println(r.area())
 }
