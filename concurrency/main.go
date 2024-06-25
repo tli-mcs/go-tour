9f16064f4e3b67e81dd5cecd5e9a423a699a7355
Ting Li <tli@macquariecloudservices.com>
2024-06-25 21:53:07 +1000

    done

--- concurrency/main.go
+++ concurrency/main.go
@@ -1,21 +1,34 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+func counter(out chan<- int) {
+	for i := 0; i < 11; i++ {
+		out <- i
+		fmt.Println("counter called, ch1 received val: ", i)
+	}
+	close(out)
+}
+
+func squarer(out chan<- int, in <-chan int) {
+
+	for i := range in {
+		fmt.Println("squarer called, ch1 is sending val: ", i)
+		fmt.Println("squarer called, ch2 is receiving val: ", i*i)
+		out <- i * i
+	}
+	close(out)
+}
+func printer(in <-chan int) {
+	for i := range in {
+		fmt.Println("ch2 sent val: ", i)
+	}
+}
 
 func main() {
-	go func() {
-		defer fmt.Println("A.defer")
-		func() {
-			defer fmt.Println("B.defer")
-			// 结束协程
-			// runtime.Goexit()
-			defer fmt.Println("C.defer")
-			fmt.Println("B")
-		}()
-		fmt.Println("A")
-	}()
-	for {
-	}
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	go counter(ch1)
+	go squarer(ch2, ch1)
+	printer(ch2)
 }
--- leetcode/main.go
+++ leetcode/main.go
@@ -1,4 +1,42 @@
 package main
 
+import "sort"
+
+type MinStack struct {
+	stack       []int
+	sortedStack []int
+}
+
+func Constructor() MinStack {
+	return MinStack{}
+}
+
+func (this *MinStack) Push(val int) {
+	this.stack = append(this.stack, val)
+	this.sortedStack = append(this.sortedStack, val)
+	sort.Ints(this.sortedStack)
+}
+
+func (this *MinStack) Pop() {
+	val := this.stack[len(this.stack)-1]
+	this.stack = this.stack[:len(this.stack)-1]
+	idx := -1
+	for i, v := range this.sortedStack {
+		if v == val {
+			idx = i
+			break
+		}
+	}
+	this.sortedStack = append(this.sortedStack[:idx], this.sortedStack[idx+1:]...)
+}
+
+func (this *MinStack) Top() int {
+	return this.stack[len(this.stack)-1]
+}
+
+func (this *MinStack) GetMin() int {
+	return this.sortedStack[0]
+}
+
 func main() {
 }
