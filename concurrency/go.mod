f2996239c6ebf0c28e05bd28ebdb03cad6a2928e
Ting Li <tli@macquariecloudservices.com>
2024-06-25 16:05:24 +1000

    done

---
+++ concurrency/go.mod
@@ -0,0 +1,3 @@
+module github.com/tli-mcs/gotour/concurrency
+
+go 1.22.3
--- leetcode/main.go
+++ leetcode/main.go
@@ -1,30 +1,4 @@
 package main
 
-import "strconv"
-
-func twoSum(nums []int, target int) []int {
-	for i, num := range nums {
-		var rem = target - num
-		for j, num2 := range nums {
-			if num2 == rem && i != j {
-				return []int{i, j}
-			}
-		}
-		// You can access each index with i and each element with num
-	}
-	return []int{}
-}
-
-func isPalindrome(x int) bool {
-	xStr := strconv.Itoa(x)
-	for i := 0; i < len(xStr)/2; i++ {
-		if xStr[i] != xStr[len(xStr)-1-i] {
-			return false
-		}
-	}
-	return true
-}
-
 func main() {
-	twoSum([]int{2, 7, 11, 15}, 9)
 }
